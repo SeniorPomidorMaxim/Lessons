70c0c313c21a3cd29e81a52ea297c991f83d3280
SeniorPomidorMaxim <bystrov_work@mail.com>
2024-01-26 23:36:23 +0500

    new_lesson

--- test.py
+++ test.py
@@ -1,63 +1,58 @@
 from pygame import *
+'''Необходимые классы'''
 
 
-window = display.set_mode((700, 500))
-display.set_caption("Догонялки")
-background = transform.scale(image.load("background.jpg"), (700, 500))
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (65, 65))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
 
 
-#данные о спрайте-картинке
-x1 = 100 #координаты 1ого спрайта
-y1 = 300
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-x2 = 300    #координаты 2ого 
-y2 = 300
+#Игровая сцена:
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
 
 
-sprite1 = transform.scale(image.load('sprite1.gif'), (100, 100)) # назначаем нашей моделе такие картинки 
-sprite2 = transform.scale(image.load('sprite2.jpg'), (100, 100)) # назначаем нашей моделе такие картинки 
-speed = 10
+#Персонажи игры:
+player = GameSprite('hero.png', 5, win_height - 80, 4)
+monster = GameSprite('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
 
-#игровой цикл
-run = True
+game = True
 clock = time.Clock()
 FPS = 60
 
 
-while run:
-   window.blit(background,(0, 0)) # отобразить фон в окне
-   window.blit(sprite1, (x1, y1))  # отобразить спрайт в таких координатах
-   window.blit(sprite2, (x2, y2)) # отобразить спрайт в таких координатах
+#музыка
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
 
 
+while game:
    for e in event.get():
-       if e.type == QUIT:   # пока не нажа красный крести игра продолжается, если нажат то все
-           run = False
-
-
-   keys_pressed = key.get_pressed()
-
-
-   if keys_pressed[K_LEFT] and x1 > 5:
-       x1 -= speed
-   if keys_pressed[K_RIGHT] and x1 < 595:
-       x1 += speed
-   if keys_pressed[K_UP] and y1 > 5:
-       y1 -= speed
-   if keys_pressed[K_DOWN] and y1 < 395:
-       y1 += speed
-
-
-   if keys_pressed[K_a] and x2 > 5:
-       x2 -= speed
-   if keys_pressed[K_d] and x2 < 595:
-       x2 += speed
-   if keys_pressed[K_w] and y2 > 5:
-       y2 -= speed
-   if keys_pressed[K_s] and y2 < 395:
-       y2 += speed
+       if e.type == QUIT:
+           game = False
+  
+   window.blit(background,(0, 0))
+   player.reset()
+   monster.reset()
 
 
    display.update()
